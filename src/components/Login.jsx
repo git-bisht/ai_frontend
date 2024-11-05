6fd243dfa0b41add3a0bfcc9ddd9cdff3a1b0a8b
Kartikeya Harsh <harshkartikeya07@gmail.com>
2024-11-05 21:40:57 +0530

    form update

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,39 +1,42 @@
 // src/components/Login.js
 import React, { useState } from 'react';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import { Container, Row, Col, Card, Form, Button } from 'react-bootstrap';
+import '../style/css/my.css';
 const Login = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-
-    const handleLogin = (e) => {
-        e.preventDefault();
-        // Here you can add authentication logic
-        console.log('Logged in with:', { username, password });
-    };
-
+   
     return (
-        <div>
-            <h2>Login</h2>
-            <form onSubmit={handleLogin}>
-                <div>
-                    <label>Username:</label>
-                    <input
-                        type="text"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <label>Password:</label>
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </div>
-                <button type="submit">Login</button>
-            </form>
-        </div>
+       
+        <Container fluid className="vh-100 d-flex justify-content-center align-items-center">
+        <Row>
+            <Col md={12}>
+                <Card className="signin">
+                    <Card.Body>
+                        <Card.Title className="text-center kr"><h1>Sign In</h1></Card.Title>
+                        <Form className='form'>
+                            <Form.Group controlId="formBasicEmail">
+                                
+                                <Form.Control type="email" placeholder="Enter email" required />
+                            </Form.Group>
+
+                            <Form.Group controlId="formBasicPassword">
+                                
+                                <Form.Control type="password" placeholder="Password" required />
+                            </Form.Group>
+                            <div className="text-center mt-3 links">
+                            <Link to="/forgot">Forgot Password</Link>
+                                <Link to="/signup">Signup</Link>
+                            </div>
+                            <Button variant="primary" type="submit" className="w-100">
+                                Submit
+                            </Button>
+                        </Form>
+                    </Card.Body>
+                </Card>
+            </Col>
+        </Row>
+    </Container>
     );
 };
 
